43af07266417a4d832303541c2d6107781b54d97
BigOlBozo <meepdamerp1@gmail.com>
2023-05-22 09:44:45 -0500

    pics

--- drone.py
+++ drone.py
@@ -5,63 +5,70 @@ from keyboard import is_pressed as press
 from time import perf_counter_ns as click
 from datetime import datetime
 os.system('cls')
-
+import time
 drone = tello.Tello()
 drone.connect()
 
 #camera
 import cv2
-import time 
-
-global img
-drone.streamon()
 
 
-def controller(starttime,stbat, now):
+
+def controller(starttime,stbat, now, img):
     lr, ud, bf, yv = 0,0,0,0
     speed = 50
+    
     if press('p'): #picture
-        cv2.imwrite(f'C:\\Users\\alfrost1@cps.edu\\Desktop\\drone\\{time.time()}.jpg', img)
+        cv2.imwrite(f'C:\\Users\\alfrost1@cps.edu\\Desktop\\drone\\{now.strftime("%m/%d/%Y.%H:%M:%S")}.jpg', img)
         sleep(0.2)
-    if press('f'):
+
+    if press('o'):
+        drone.streamon()
+
+    if press('f'): 
         drone.takeoff()
-    if press('w'):
+    if press('g'):
+        drone.land()
+
+    if press('w'): 
         print('forward')
         ud = 200
     if press('s'):
         print('back')
         ud = -200
+
     if press('a'):
         print('left')
         lr = -speed
     if press('d'):
         print('right')
         lr = speed
+
     if press('space'):
         print('up')
         bf = speed
     if press('shift'):
         print('down')
         bf = -speed
+
     if press('e'):
         yv = 200
     if press('q'):
         yv = -200
-    if press('g'):
-        drone.land()
-        print('land')
-    if press('i'):
+    
+    if press('up'):
         drone.flip('f')
-        sleep(.5)
-    if press('k'):
+        sleep(0.3)
+    if press('down'):
         drone.flip('b')
-        sleep(.5)
-    if press('j'):
+        sleep(0.3)
+    if press('left'):
         drone.flip('l')
-        sleep(.5)
-    if press('l'):
+        sleep(0.3)
+    if press('right'):
         drone.flip('r')
-        sleep(.5)
+        sleep(0.3)
+    
     return [lr,ud,bf,yv]
 
 def run():
@@ -73,17 +80,15 @@ def run():
     while True:
         if press('enter'): #kill switch
             drone.land()
+            drone.streamoff()
             break
-        vals = controller(starttime,stbat,now)
+        img = drone.get_frame_read().frame
+        img = cv2.resize(img, (720,480))
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+        
+        vals = controller(starttime,stbat,now,img)
         drone.send_rc_control(vals[0],vals[1],vals[2],vals[3])
-        try: #img
-            img = drone.get_frame_read().frame
-            img = cv2.resize(img, (360,240))
-            cv2.imshow("Image", img)
-            cv2.waitKey(1)
-        except:
-            print('Error')
-            pass
     #log 
     doc = open('log.txt','a')
     doc.write(f'{now.strftime("%m/%d/%Y %H:%M:%S")} Flight Time: {(click()-starttime)/1000000000} Battery: {drone.get_battery()} Battery Loss:{drone.get_battery()-stbat}\n')
---
+++ test.py
@@ -0,0 +1,4 @@
+from keyboard import is_pressed as press
+x = 0
+while True:
+    if press('p'): print('press') x = 1
